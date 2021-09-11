b015c5e83bd31b731e75b2acb6bfd16d8f448201
git2020ybq <git2020_ybq@aliyun.com>
2021-09-11 13:38:35 +0800

    小袁修改后提交并提交远程仓库

--- src/main/java/com/wj/git/TestGit.java
+++ src/main/java/com/wj/git/TestGit.java
@@ -2,6 +2,8 @@ package com.wj.git;
 
 public class TestGit {
     public static void main(String[] args) {
+
         System.out.println("Test Git");
+        System.out.println("hello,git");
     }
 }
